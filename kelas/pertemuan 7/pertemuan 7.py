50d70886db0fad7a53e842597c2d2450604394da
Auliawak <auliarachmancuy@gmail.com>
2024-10-18 14:55:45 +0800

    Add files via upload

---
+++ kelas/pertemuan 7/pertemuan 7.py
@@ -0,0 +1,81 @@
+# def aku_fungsi ():
+#     print("Hello world")
+
+
+
+# aku_fungsi()
+
+# def tambah(a,b):
+#     hasil = a+b
+
+#     print(hasil)
+
+# tambah(10,2)
+
+# def tambah(a,b):
+#     hasil = a+b
+#     return hasil
+
+# print(tambah(2,4))
+
+# def tambah(a,b):
+#     hasil = a+b
+#     return hasil
+
+# tambahan = tambah(4,2) + tambah(2,3)
+# print(tambahan)
+
+# def pilihan(opsi):
+#     match opsi:
+#         case 1:
+#             print(1)
+#             #return
+#         case 2:
+#             print(2)
+#             #return
+#     print(3)
+
+# pilihan(2)
+
+# def cetak(nama, nim, matkul):
+#     print(nama,nim,matkul)
+
+# cetak("fathir",41,"kalkulus")
+
+# var = 2,4,5
+
+# # print(type(var))
+
+# var1, var2, var3 = var
+#Nama = "Teknik Elektro"
+#     
+# print(var)
+# print(var1)
+# print(var2)
+
+# Nama = "Informatika"
+# Mata_Kuliah = "Algoritma dan Pemrograman Dasar"
+# kelas = "IT asikk"
+# # membuat variabel lokal
+# def info():
+#     Mata_Kuliah = "Pengantar Teknik ELektro"
+#     var = 1000
+#     # mengakses variabel lokal
+#     print("Prodi:", Nama)
+#     print("Mata Kuliah:", Mata_Kuliah)
+#     print(kelas)
+
+# info()
+# # print("Prodi:", Nama)
+# # print("Mata Kuliah:", Mata_Kuliah)
+# print(var)
+
+
+# def cetak(nama, nim, umur = 18):
+#     print(nama,nim,umur)
+# cetak("fathir",41)
+
+
+# if __name__ == "__main__":
+#     def cetak (nama, nim, umur = 17)
+#         print(nama,nim,umur)
